adafd95b6875a29765a242b9c7686449eb72ea4f
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-20 18:40:54 +0300

    added data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+class Endpoints:
+    CREATE_USER = '/api/auth/register'
+    LOGIN_USER = '/api/auth/login'
+    UPDATE_USER = '/api/auth/user'
+    CREATE_ORDER = '/api/orders'
+    GET_ORDERS_USER = '/api/orders'
