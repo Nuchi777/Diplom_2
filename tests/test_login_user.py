2377f634654c62897096e5fb8fadc0b175736180
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-25 18:14:35 +0300

    added tests login user

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -1,10 +1,52 @@
 import allure
-import pytest
 import requests
 from faker import Faker
 from data import Urls, Endpoints
+from random import randint
 
 faker = Faker()
 
 
-class TestRegistrationUser:
+class TestLoginUser:
+    @allure.suite("Логин пользователя:")
+    @allure.title("Логин под существующим пользователем")
+    def test_login_registered_user(self, register_new_user_return_login_pass):
+        data = register_new_user_return_login_pass
+        email = data[0]
+        password = data[1]
+        payload = {
+            "email": email,
+            "password": password
+        }
+        response = requests.post(f'{Urls.URL_SB}{Endpoints.LOGIN_USER}', data=payload)
+        email = response.json()["user"]["email"]
+        name = response.json()["user"]["name"]
+        access_token = response.json()["accessToken"]
+        refresh_token = response.json()["refreshToken"]
+        assert response.status_code == 200 and response.text == f'{{"success":true,"accessToken":"{access_token}","refreshToken":"{refresh_token}","user":{{"email":"{email}","name":"{name}"}}}}'
+
+    @allure.title("Логин с неверным логином и верным паролем")
+    def test_login_user_with_incorrect_username_and_correct_password(self, register_new_user_return_login_pass):
+        data = register_new_user_return_login_pass
+        email = faker.free_email()
+        password = data[1]
+        payload = {
+            "email": email,
+            "password": password
+        }
+        response = requests.post(f'{Urls.URL_SB}{Endpoints.LOGIN_USER}', data=payload)
+        assert response.status_code == 401 and response.text == '{"success":false,"message":"email or password are incorrect"}'
+
+    @allure.title("Логин с верным логином и неверным паролем")
+    def test_login_user_with_correct_username_and_incorrect_password(self, register_new_user_return_login_pass):
+        data = register_new_user_return_login_pass
+        email = data[0]
+        password = randint(100000, 999999)
+        payload = {
+            "email": email,
+            "password": password
+        }
+        response = requests.post(f'{Urls.URL_SB}{Endpoints.LOGIN_USER}', data=payload)
+        assert response.status_code == 401 and response.text == '{"success":false,"message":"email or password are incorrect"}'
+
+
