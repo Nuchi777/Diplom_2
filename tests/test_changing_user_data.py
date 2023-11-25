c743ed109a1f3f38072843605dca6522e0f683f3
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-25 21:31:37 +0300

    fix test_changing_user_data_without_authorization

--- tests/test_changing_user_data.py
+++ tests/test_changing_user_data.py
@@ -20,8 +20,10 @@ class TestChangingUserData:
         name = response.json()["user"]["name"]
         assert response.status_code == 200 and response.text == f'{{"success":true,"user":{{"email":"{email}","name":"{name}"}}}}'
 
+    @pytest.mark.parametrize('payload_data',
+                             [f'{{"name": "{faker.first_name()}"}}', f'{{"email": "{faker.free_email()}"}}'])
     @allure.title("Изменение данных пользователя без авторизации")
-    def test_changing_user_data_without_authorization(self, register_new_user_return_response):
-        payload = {"name": "Ivan Ivanov"}
+    def test_changing_user_data_without_authorization(self, register_new_user_return_response, payload_data):
+        payload = payload_data
         response = requests.patch(f'{Urls.URL_SB}{Endpoints.UPDATE_USER}', data=payload)
         assert response.status_code == 401 and response.text == '{"success":false,"message":"You should be authorised"}'
