507389f11339f5cd74daba5864acc30f6c50b41a
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-11-25 19:09:40 +0300

    added test_changing_user_data_without_authorization

--- tests/test_changing_user_data.py
+++ tests/test_changing_user_data.py
@@ -18,10 +18,7 @@ class TestChangingUserData:
 
     @allure.title("Изменение данных пользователя без авторизации")
     def test_changing_user_data_without_authorization(self, register_new_user_return_response):
-        data = register_new_user_return_response
-        email = data.json()["user"]["email"]
-        access_token = data.json()["accessToken"]
         payload = {"name": "Ivan Ivanov"}
-        response = requests.patch(f'{Urls.URL_SB}{Endpoints.UPDATE_USER}', data=payload, headers={'Authorization': f'{access_token}'})
-        assert response.status_code == 200 and response.text == f'{{"success":true,"user":{{"email":"{email}","name":"Ivan Ivanov"}}}}'
+        response = requests.patch(f'{Urls.URL_SB}{Endpoints.UPDATE_USER}', data=payload)
+        assert response.status_code == 401 and response.text == '{"success":false,"message":"You should be authorised"}'
 
